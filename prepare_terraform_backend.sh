b97c6d2d6d116c4337834994e0a493f9d8369281
Afraz Ahmadzadeh <afrazo@protonmail.com>
2023-05-23 11:43:46 +0200

    Create backend.tf
    `prepare_terraform_backend.sh` script will now create the `backend.tf`
    file, rather than needing a file to copy in the repository.

--- prepare_terraform_backend.sh
+++ prepare_terraform_backend.sh
@@ -8,15 +8,17 @@ if [[ "$CI" != "" ]];then
 fi
 
 if [ -z "$1" ];then
-  echo "Please provde the project name as the first argument (e.g. 'web'"
-  echo "Hint:"
+  echo "⚠️  Please provde the project name as the first argument (e.g. 'web'"
+  echo "ℹ️  Hint: It's the firs bit of a bucket ending with '-dev-terraform-backends'"
   aws s3 ls
   exit 1
 fi
-BUCKET="$1-dev-terraform-backends"
+PROJECT=$1
+BUCKET="$PROJECT-dev-terraform-backends"
 
 if [ -z "$2" ];then
-  echo "Please provde one of these for the 'workspace' name as the second argument:"
+  echo "⚠️  Please provde one of these for the 'workspace' name as the second argument:"
+  echo "ℹ️"
   aws s3 ls "s3://$BUCKET/"
   exit 1
 fi
@@ -24,11 +26,36 @@ S3_WORKSPACE=$2
 
 AWS_SESSION=$(leapp session current | jq -r '.alias')
 if [[ "$AWS_SESSION" == "" ]]; then
-  echo 'Something went wrong whilst trying to get your current AWS session. Are you connected to LEAPP?'
+  echo '⚠️  Something went wrong whilst trying to get your current AWS session. Are you connected to LEAPP?'
   exit 1
 fi
 
 aws s3 cp "s3://$BUCKET/$S3_WORKSPACE/terraform.tfvars" .
-echo "Copied variables file to terraform.tfvars"
-cp terraform-local-configuration backend.tf
-echo "Copied backend (S3) configuration to backend.tf"
+echo "🤘 Copied variables file to terraform.tfvars"
+
+echo "
+terraform {
+  backend \"s3\" {
+    bucket         = \"$BUCKET\"
+    key            = \"$S3_WORKSPACE/afsprk_nl.tfstate\"
+    region         = \"eu-central-1\"
+    dynamodb_table = \"$PROJECT-dev-terraform-backends-statefile-locks\"
+    encrypt        = true
+  }
+}
+" > backend.tf
+echo "🤘 Created backend (S3) configuration as 'backend.tf'"
+
+if ! [[ -f ".gitignore" ]]; then
+  touch .gitignore
+  echo "🤘 Created .gitignore file, because one didn't exist"
+fi
+
+IGNORED_FILES=( "node_modules" "dist" "coverage/" ".npmrc" ".DS_Store" "config/local.json" ".terraform" ".terraform.lock.hcl" "terraform.tfstate*" "*.tfvars" "backend.tf")
+for this_filename in "${IGNORED_FILES[@]}"
+do :
+  if ! grep -q "$this_filename" '.gitignore'; then
+    echo "$this_filename" >> .gitignore
+    echo "🤘 Added $this_filename to .gitignore because it was missing"
+  fi
+done
